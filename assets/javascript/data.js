0d07c2fd50f527b82600eadf6ff92ca336845fe7
Dang Huy <phandanghuy300305@gmail.com>
2024-10-25 14:03:05 +0700

    lần 8

---
+++ assets/javascript/data.js
@@ -0,0 +1,21 @@
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+  host: "your_host",
+  user: "your_user",
+  password: "your_password",
+  database: "your_database",
+});
+
+connection.connect((err) => {
+  if (err) throw err;
+  console.log("Connected   to database!");
+
+  const sql = "SELECT * FROM users";
+  connection.query(sql, (err, results) => {
+    if (err) throw err;
+    console.log(results);
+  });
+
+  connection.end();
+});
