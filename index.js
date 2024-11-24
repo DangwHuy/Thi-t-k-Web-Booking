3c02c61b3c2f93a7dc51f91413edb1016ec127d8
Dang Huy <phandanghuy300305@gmail.com>
2024-11-24 17:15:28 +0700

    theem khachsan

--- index.js
+++ index.js
@@ -14,3 +14,43 @@ tabs.forEach((tab) => {
       .classList.add("active");
   });
 });
+// ------------------------------------Khách hàng noí gì?------------
+const next = document.getElementById("sau");
+const prev = document.getElementById("truoc");
+const khachHangThe = Array.from(document.querySelectorAll(".khach__hang__the"));
+
+next.addEventListener("click", () => {
+  for (let index = 0; index < khachHangThe.length; index++) {
+    if (khachHangThe[index].classList.contains("hoat__dong")) {
+      const nextIndex = (index + 1) % khachHangThe.length;
+      khachHangThe[index].classList.remove("hoat__dong");
+      khachHangThe[nextIndex].classList.add("hoat__dong");
+      break;
+    }
+  }
+});
+
+prev.addEventListener("click", () => {
+  for (let index = 0; index < khachHangThe.length; index++) {
+    if (khachHangThe[index].classList.contains("hoat__dong")) {
+      const prevIndex = (index ? index : khachHangThe.length) - 1;
+      khachHangThe[index].classList.remove("hoat__dong");
+      khachHangThe[prevIndex].classList.add("hoat__dong");
+      break;
+    }
+  }
+});
+
+// Hiệu ứng cuộn cho phần đăng ký
+ScrollReveal().reveal(".dang__ky__muc .phan__muc__tieu__de", {
+  ...scrollRevealOption,
+});
+ScrollReveal().reveal(".dang__ky__muc .phan__muc__mo__ta", {
+  ...scrollRevealOption,
+  delay: 500,
+});
+ScrollReveal().reveal(".dang__ky__muc form", {
+  ...scrollRevealOption,
+  delay: 1000,
+});
+// ------------------------------------Khách hàng noí gì?------------
